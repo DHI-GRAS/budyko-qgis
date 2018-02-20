6f3a645675062cad0d0a1c6aad75f7a76d280e9a
Jonas Sølvsteen <j08lue@gmail.com>
2018-02-20 11:03:41 +0100

    add parameters

--- budyko_qgis/qgis_script.py
+++ budyko_qgis/qgis_script.py
@@ -1,12 +1,27 @@
 ##Budyko=group
 ##Budyko Hydrological Model=name
-##ParameterFile|GEOMETRY_FILE|
+##ParameterFile|GEOMETRY_FILE|Geometry setup
+##ParameterFile|MODEL_FILE|Model setup
 ##ParameterFile|OBS_REACH_FILE|Observed-discharge file|False|True
-##ParameterFile|OUTDIR|Output directory for plots|True|True
+##ParameterFile|PARAMETER_FILE|Model parameter file|False|True
+##ParameterNumber|REACH_NUMBER|Reach number considered|0
+##ParameterString|STARTDATE|Start date YYYYJJJ|
+##ParameterSelection|OUTPUT_TYPE|Output type|Hydrograph;Climatology;FDC|0
+##OutputDirectory|OUTDIR|Output directory for plots
+##ParameterSelection|TIME_STAMP|Time resolution|daily;monthly|0
+##*ParameterNumber|N_CLASSES|Number of volume classes for FDC|0|1000|20
+##*ParameterString|TITLE|Title for plot|||True
 
-from middle_layer import main
+from budyko_model.scripts import middle_layer
 
-main(
+middle_layer.main(
+    model_file=MODEL_FILE,
+    parameter_file=PARAMETER_FILE,
     obs_reach_file=OBS_REACH_FILE,
     outdir=OUTDIR,
-)
+    reach_number=REACH_NUMBER,
+    startdate=STARTDATE,
+    output_type=OUTPUT_TYPE,
+    time_stamp=TIME_STAMP,
+    n_classes=N_CLASSES,
+    title=TITLE)
