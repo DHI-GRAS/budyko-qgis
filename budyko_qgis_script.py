77b3ba0db8e7160bb17c4d1f7acd702eb65890d8
Jonas Sølvsteen <j08lue@gmail.com>
2018-02-21 16:01:17 +0100

    redirect stdout

--- budyko_qgis_script.py
+++ budyko_qgis_script.py
@@ -11,18 +11,41 @@
 ##ParameterSelection|TIME_STAMP|Time resolution|daily;monthly|0
 ##*ParameterNumber|N_CLASSES|Number of volume classes for FDC|0|1000|20
 ##*ParameterString|TITLE|Title for plot|||True
+import sys
+from contextlib import contextmanager
 
 from budyko_model.scripts import middle_layer
 
-middle_layer.main(
-    geometry_file=GEOMETRY_FILE,
-    model_file=MODEL_FILE,
-    parameter_file=PARAMETER_FILE,
-    obs_reach_file=OBS_REACH_FILE,
-    outdir=OUTDIR,
-    reach_number=REACH_NUMBER,
-    startdate=STARTDATE,
-    output_type=OUTPUT_TYPE,
-    time_stamp=TIME_STAMP,
-    n_classes=N_CLASSES,
-    title=TITLE)
+
+class ProgressLogger:
+
+    def __init__(self, progress):
+        self.progress = progress
+
+    def write(self, msg):
+        self.progress.setConsoleInfo(msg)
+
+
+@contextmanager
+def redirect_stdout(progress):
+    oldout = sys.stdout
+    sys.stdout = ProgressLogger(progress)
+    try:
+        yield
+    finally:
+        sys.stdout = oldout
+
+
+with redirect_stdout(progress):
+    middle_layer.main(
+        geometry_file=GEOMETRY_FILE,
+        model_file=MODEL_FILE,
+        parameter_file=PARAMETER_FILE,
+        obs_reach_file=OBS_REACH_FILE,
+        outdir=OUTDIR,
+        reach_number=REACH_NUMBER,
+        startdate=STARTDATE,
+        output_type=OUTPUT_TYPE,
+        time_stamp=TIME_STAMP,
+        n_classes=N_CLASSES,
+        title=TITLE)
