0d01e273893ceb00c712976aab2f4324ae823ac7
Radoslaw Guzinski <radosuav@op.pl>
2019-04-05 15:55:08 +0200

    Add script to prepare subreaches' geometry file.

---
+++ budyko_geometry_file_creation.py
@@ -0,0 +1,50 @@
+##Budyko=group
+##Prepare subreaches' geometry file=name
+##ParameterFile|MODEL_FILE|Model setup file|False|False
+##OutputFile|GEOMETRY_FILE|Output file with model subreaches' geometry|txt
+
+import pandas as pd
+import sys
+from contextlib import contextmanager
+
+from processing.tools.system import getTempFilename
+
+from budyko_model import geometry_file_creation
+from budyko_model.river_network import RiverNetwork
+from budyko_model.modelfile import ModelFile
+
+
+class ProgressLogger:
+
+    def __init__(self, progress):
+        self.progress = progress
+
+    def write(self, msg):
+        self.progress.setConsoleInfo(msg)
+
+
+@contextmanager
+def redirect_stdout(progress):
+    oldout = sys.stdout
+    sys.stdout = ProgressLogger(progress)
+    try:
+        yield
+    finally:
+        sys.stdout = oldout
+
+
+with redirect_stdout(progress):
+
+    routing = RiverNetwork(MODEL_FILE)
+    model = ModelFile(MODEL_FILE)
+
+    # Get drainage network
+    drains_to = routing.drains_to
+
+    temp_file = getTempFilename("csv")
+    geometry_file_creation.read_network_file(model, routing, temp_file)
+    subreaches = pd.read_csv(temp_file, index_col='Subbasin')
+    geometry_file_creation.write_geometry_file(subreaches=subreaches,
+                                               mode=model,
+                                               drains_to=drains_to,
+                                               geometry_file=GEOMETRY_FILE)
